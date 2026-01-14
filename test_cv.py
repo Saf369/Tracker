7db2770bfdf2d0de1f3190ba6d2a9e86b5d6d3a5
Nisthul-24 <nisthulk24@gmail.com>
2026-01-14 17:18:53 +0530

    first commit

---
+++ test_cv.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("OpenCV version:", cv2.__version__)
+print("NumPy version:", np.__version__)
+print("All libraries installed successfully!")
